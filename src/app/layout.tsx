6d078154b1ac783486dc47b0f8d3ed9ec66f223a
Brian Steele <brian.steele@hey.com>
2023-07-23 10:19:07 -0600

    update description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
   title: "Jackie and Brian",
-  description: "Life on bikes",
+  description: "Wedding Highlights",
 }
 
 export default function RootLayout({
